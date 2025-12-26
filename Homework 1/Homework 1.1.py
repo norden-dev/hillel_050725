9d7dcbb1a58e64b17c50378c129f026941441418
norden-dev <vlademark8@gmail.com>
2025-12-26 15:17:15 +0100

    1 commit

--- Homework 1/Homework 1.1.py
+++ Homework 1/Homework 1.1.py
@@ -1,6 +1,10 @@
 import math
 
-def calculate_circle_area(radius):
+def calculate_circle_area(radius: int|float)-> float:
+    """
+    Calculate the area of a circle given its radius.
+    Returns:
+        float: The area of the circle """
     return math.pi * radius**2
 
 radius = float(input("Input radius: "))
