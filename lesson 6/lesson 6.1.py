d216df0d7f77cadc3f28d726e973754a8cdbe5bc
norden-dev <vlademark8@gmail.com>
2025-12-26 13:55:41 +0100

    Delete lesson 6 directory

--- lesson 6/lesson 6.1.py
+++
@@ -1,6 +0,0 @@
-
-import string
-first_letter, last_letter = input("ВВедите через -").split("-")
-letter = string.ascii_letters
-print(letter[letter.index(first_letter) : letter.index(last_letter) +1])
-
