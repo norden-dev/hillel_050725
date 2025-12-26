e8ed65553311c7fc33800f5074d95f4845f9f4c1
norden-dev <vlademark8@gmail.com>
2025-12-26 16:51:30 +0100

    1 commit

--- Homework 1/Homework 1.2.py
+++ Homework 1/Homework 1.2.py
@@ -1,1 +1,30 @@
-f
+class Rectangle:
+
+    def __init__( self, width: float, height: float) -> None:
+        self.width = width
+        self.height = height
+
+    def area(self)-> float:
+        """Calculates the area of a rectangle."""
+        return self.width * self.height
+
+    def perimetr(self)-> float:
+        """Calculates perimetr of a rectangle"""
+        return 2* (self.width + self.height)
+
+    def is_square(self)->bool:
+        """Checking for square or not"""
+        return True if self.width == self.height else False
+
+    def resize(self, new_width, new_height) -> None:
+        """Resize width and height"""
+        self.width = new_width
+        self.height = new_height
+
+
+react = Rectangle(2, 4 )
+print(react.area())
+print(react.perimetr())
+print(react.is_square())
+print(react.resize(5,3))
+print(react.width, react.height)
